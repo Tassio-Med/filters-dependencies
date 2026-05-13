6681c7e569831330f1080da4dfd7ee8aebbbeb09
Tássio Medeiros <tassiohfmed@gmail.com>
2026-05-13 16:45:26 -0300

    feat: cria chaves para cache no redux

---
+++ src/app/shared/constants/cacheKeys.js
@@ -0,0 +1,8 @@
+export const CACHE_KEYS = {
+    USUARIOS: '@cache/usuarios',
+    ALBUMS: '@cache/albuns',
+    POSTS: '@cache/posts',
+    COMENTARIOS: '@cache/comentarios'
+};
+
+export  default CACHE_KEYS;
