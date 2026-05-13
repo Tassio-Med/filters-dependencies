95a4d1ab8afe67ba3757171551b105812fa75ba8
Tássio Medeiros <tassiohfmed@gmail.com>
2026-05-13 16:29:02 -0300

    feat: cria const globais

---
+++ src/app/shared/constants/AppConfigKeys.js
@@ -0,0 +1,16 @@
+export const APP_CONFIG = {
+    LIMIT_PADRAO: 10,
+    TIMEOUT_PADRAO: 30000,
+    MAX_RETRIES: 3,
+
+    FILTRO_TODOS: -1,
+    FILTRO_VAZIO: '',
+
+    STATUS: {
+        ATIVO: 1,
+        INATIVO: 0,
+        TODOS: -1,
+    }
+}
+
+export default APP_CONFIG;
