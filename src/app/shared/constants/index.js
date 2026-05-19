0433ae62ecf1cfaef1ec8f7b7de92e885800d141
Tássio Medeiros <tassiohfmed@gmail.com>
2026-05-19 10:28:02 -0300

    feat: centraliza exportações das constantes

---
+++ src/app/shared/constants/index.js
@@ -0,0 +1,7 @@
+export * from './ApiRoutes';
+export * from './AppConfigKeys';
+export * from './cacheKeys';
+
+export const FILTRO_TODOS = -1;
+export const FILTRO_VAZIO = '';
+export const FILTRO_PADRAO = 10;
