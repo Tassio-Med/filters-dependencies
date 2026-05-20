301dc6a9f7a8faca43601b516205187f66ebe4ac
Tássio Medeiros <tassiohfmed@gmail.com>
2026-05-20 16:16:55 -0300

    refactor: corrige funções ajusta estilização do código

--- src/app/shared/helpers/validateHelpers.js
+++ src/app/shared/helpers/validateHelpers.js
@@ -1,61 +1,72 @@
 export const isNotEmpty = (value) => {
-    return value !== null & value !== undefined  && value !== '';
+  return value !== null && value !== undefined && value !== '';
 };
+
 
 export const isNumber = (value) => {
-    return !isNaN(parseFloat(value)) && isFinite(value);
+  return !isNaN(parseFloat(value)) && isFinite(value);
 };
+
 
 export const isInteger = (value) => {
-    return isNumber(value) && parseInt(value) === parseFloat(value);
+  return isNumber(value) && parseInt(value) === parseFloat(value);
 };
+
 
 export const isValidEmail = (email) => {
-    const regex = /^[^\s@]+@([^\s@]+\.)+[^\s@]+$/;
-    return regex.test(email);
+  const regex = /^[^\s@]+@([^\s@]+\.)+[^\s@]+$/;
+  return regex.test(email);
 };
+
 
 export const isPositiveNumber = (value) => {
-    return isNumber(value) && parseFloat(value) > 0;
+  return isNumber(value) && parseFloat(value) > 0;
 };
+
 
 export const isValidId = (id) => {
-    return id & id !== -1 && isPositiveNumber(id);
+  return id && id !== -1 && isPositiveNumber(id);
 };
 
-export const hasMinLength = (value, minLength = 1) = () => {
-    if(!value) return false;
-    return value.toString().length >= minLength;
+
+export const hasMinLength = (value, minLength = 1) => {
+  if (!value) return false;
+  return value.toString().length >= minLength;
 };
 
-export const hasMaxLength = (value, maxLength = 1) = () => {
-    if(!value) return true;
-    return value.toString().length <= maxLength;
+
+export const hasMaxLength = (value, maxLength = 255) => {
+  if (!value) return true;
+  return value.toString().length <= maxLength;
 };
+
 
 export const isValidDate = (date) => {
-    const d = new Date(date);
-    return d instanceof Date && !isNaN(d);
+  const d = new Date(date);
+  return d instanceof Date && !isNaN(d);
 };
+
 
 export const isEmptyObject = (obj) => {
-    return obj && Object.keys(obj).length === 0 && obj.costructor === Object;
+  return obj && Object.keys(obj).length === 0 && obj.constructor === Object;
 };
+
 
 export const isEmptyArray = (arr) => {
-    return !arr || !Array.isArray(arr) || arr.length === 0;
+  return !arr || !Array.isArray(arr) || arr.length === 0;
 };
 
+
 export default {
-    isNotEmpty,
-    isNumber,
-    isInteger,
-    isValidEmail,
-    isPositiveNumber,
-    isValidId,
-    hasMaxLength,
-    hasMinLength,
-    isValidDate,
-    isEmptyObject,
-    isEmptyArray,
+  isNotEmpty,
+  isNumber,
+  isInteger,
+  isValidEmail,
+  isPositiveNumber,
+  isValidId,
+  hasMinLength,
+  hasMaxLength,
+  isValidDate,
+  isEmptyObject,
+  isEmptyArray,
 };
