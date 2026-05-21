df13615d2bde9d5c2470d9cf0a6eaedb8e7d2eae
Tássio Medeiros <tassiohfmed@gmail.com>
2026-05-21 16:17:46 -0300

    feat: criar arquivo de exportação dos controllers

---
+++ src/controllers/index.js
@@ -0,0 +1,3 @@
+export { default as UsuarioController } from './UsuarioCOntroller';
+export { default as AlbumController } from './AlbumController';
+export { default as PostController } from './PostController';
